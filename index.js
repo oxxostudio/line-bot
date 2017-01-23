450988d65a98aae2dd45338f7e0c80008df5d145
oxxo <oxxo.studio@gmail.com>
2017-01-23 16:26:28 +0800

    add webduino

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var getJSON = require('get-json');
 var request = require("request");
 var cheerio = require("cheerio");
 var httpp = require("http");
+require("webduino-js");
+require("webduino-blockly");
 
 var bot = linebot({
   channelId: '1496768068',
@@ -16,6 +18,7 @@ var jp;
 var pm = [];
 var botEvent;
 var replyMsg;
+var deviceId = '10RrGGer';
 var words = [{
   "name": "pm25",
   "content": ['pm2.5', 'PM2.5', 'PM25', 'pm25', '空氣污染', '空汙', '空氣品質', 'PM10', 'pm10']
@@ -25,6 +28,12 @@ var words = [{
 }, {
   "name": "japan",
   "content": ['日幣', '日圓', '日元']
+}, {
+  "name": "color",
+  "content": ['紅色', '藍色', '綠色', '黃色', '關燈', '開燈']
+}, {
+  "name": "device",
+  "content": ['裝置ID:', '裝置id:', '裝置 ID:', '裝置 id:', 'device id:', 'Device id:']
 }];
 var a0 = 0;
 
@@ -51,11 +60,15 @@ function _bot() {
       words.forEach(function(row) {
         row.content.forEach(function(col) {
           if (msg.indexOf(col) != -1) {
-          	a0 = 1;
+            a0 = 1;
             if (row.name == 'pm25' || row.name == 'pm25Location') {
               _pm25(msg);
             } else if (row.name == 'japan') {
               _japan();
+            } else if (row.name == 'device') {
+              _deviceId(msg);
+            } else if (row.name == 'color') {
+              _webduino(msg);
             }
           }
         });
@@ -142,6 +155,56 @@ function _japan() {
       });
       a0 = 0;
     }
+  });
+}
+
+function _deviceId(msg) {
+  var d = msg.split(':')[1];
+  if (d) {
+    deviceId = d;
+  }
+  replyMsg = '新的 Device ID [ '+d+' ] 設定完成';
+  botEvent.reply(replyMsg).then(function(data) {
+    console.log(replyMsg);
+  }).catch(function(error) {
+    console.log('error');
+  });
+}
+
+function _webduino(msg) {
+  var rgbled;
+  boardReady({
+    board: 'Smart',
+    device: deviceId,
+    transport: 'mqtt'
+  }, function(board) {
+    board.systemReset();
+    board.samplingInterval = 50;
+    rgbled = getRGBLedCathode(board, 15, 12, 13);
+    if (msg.indexOf('黃色')!=-1) {
+      rgbled.setColor('#ffff00');
+      replyMsg = '已經發出黃色光...';
+    } else if (msg.indexOf('紅色')!=-1) {
+      rgbled.setColor('#ff0000');
+      replyMsg = '已經發出紅色光...';
+    } else if (msg.indexOf('綠色')!=-1) {
+      rgbled.setColor('#00ff00');
+      replyMsg = '已經發出綠色光...';
+    } else if (msg.indexOf('藍色')!=-1) {
+      rgbled.setColor('#0000ff');
+      replyMsg = '已經發出藍色光...';
+    } else if (msg.indexOf('開燈')!=-1) {
+      rgbled.setColor('#ffffff');
+      replyMsg = '開燈囉！';
+    } else if (msg.indexOf('關燈')!=-1) {
+      rgbled.setColor('#000000');
+      replyMsg = '關起來了！';
+    }
+    botEvent.reply(replyMsg).then(function(data) {
+      console.log(replyMsg);
+    }).catch(function(error) {
+      console.log('error');
+    });
   });
 }
 
